27f51ecd64ae2cfb80e5c0a11af51360a50202f5
John Sell <jsell@redhat.com>
2025-10-17 15:56:54 -0400

    fix: Enable class prop for DropdownMenuTrigger component
    - Add class prop to DropdownMenuTrigger component type definition
    - Import and use cn utility to apply classes
    - This allows styling to be applied to the trigger button

--- web/app/components/ui/dropdown-menu/DropdownMenuTrigger.vue
+++ web/app/components/ui/dropdown-menu/DropdownMenuTrigger.vue
@@ -1,11 +1,16 @@
 <script setup lang="ts">
 import { DropdownMenuTrigger, type DropdownMenuTriggerProps } from 'radix-vue'
+import { cn } from '~/lib/utils'
 
-const props = defineProps<DropdownMenuTriggerProps>()
+const props = defineProps<
+  DropdownMenuTriggerProps & {
+    class?: string
+  }
+>()
 </script>
 
 <template>
-  <DropdownMenuTrigger v-bind="props">
+  <DropdownMenuTrigger v-bind="props" :class="cn(props.class)">
     <slot />
   </DropdownMenuTrigger>
 </template>
